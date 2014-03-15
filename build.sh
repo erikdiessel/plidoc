e5155801dfe489261992222deae0f4e2876f5ff8
erikdiessel <erik.diessel@gmx.de>
2014-03-15 20:29:10 +0000

    Changed buildscript for travis-ci

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 # compile
 coffee -c docco.litcoffee;
-# install
+# install globally and locally (for continuos integration)
 npm install -g
+npm install
 # run on test files
 docco -l linear -o test/fixtures/docs test/fixtures/testSourceFile.js
 docco -l linear -o test/fixtures/docs test/fixtures/testSourceFileEndingWithDescription.js
